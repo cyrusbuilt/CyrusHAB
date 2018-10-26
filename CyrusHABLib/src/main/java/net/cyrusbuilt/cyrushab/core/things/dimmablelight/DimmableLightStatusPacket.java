b4606537289964a9bebe3a144795e485f4b394b3
Chris Brunner <cyrusbuilt@gmail.com>
2018-10-26 12:20:44 -0400

    Fixing build tasks, adding CI, building out API

---
+++ CyrusHABLib/src/main/java/net/cyrusbuilt/cyrushab/core/things/dimmablelight/DimmableLightStatusPacket.java
@@ -0,0 +1,4 @@
+package net.cyrusbuilt.cyrushab.core.things.dimmablelight;
+
+public class DimmableLightStatusPacket {
+}
---
+++ CyrusHABLib/src/main/java/net/cyrusbuilt/cyrushab/core/things/switches/SwitchControlPacket.java
@@ -0,0 +1,4 @@
+package net.cyrusbuilt.cyrushab.core.things.switches;
+
+public class SwitchControlPacket {
+}
